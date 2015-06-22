898e64eb1627a12e621d0bf570eeb4a4700e95e5
Alp Eren Can <alperenc@gmail.com>
2015-06-22 20:24:50 +0300

    Initial Commit

---
+++ Car/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Car
+//
+//  Created by Alp Eren Can on 22/06/15.
+//  Copyright (c) 2015 Alp Eren Can. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
